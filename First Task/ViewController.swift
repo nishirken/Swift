3c3649585ec1045a96a13a4192515ddb5401da46
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-01-31 15:27:11 +0300

    Initial Commit

---
+++ First Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Task
+//
+//  Created by Дмитрий Скурихин on 31.01.2019.
+//  Copyright © 2019 Дмитрий Скурихин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
