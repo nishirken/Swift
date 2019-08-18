59f93aa9e543c6195500081a16e12e7a1a99448b
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-08-18 23:09:01 +0300

    complete first task

---
+++ FirstTask/View.swift
@@ -0,0 +1,13 @@
+//
+//  View.swift
+//  FirstTask
+//
+//  Created by Shadow on 23/08/2019.
+//  Copyright © 2019 Дмитрий Скурихин. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+    
+}
--- FirstTask/ViewController.swift
+++ FirstTask/ViewController.swift
@@ -9,64 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var firstOperandResultLabel: UILabel!
-    var secondOperandResultLabel: UILabel!
+    var firstOperand: UILabel!
+    var secondOperand: UILabel!
     var segmentedControl: UIStepper!
     var slider: UISlider!
     var calculateButton: UIButton!
     var resultLabel: UILabel!
-    let resultFormatter: NumberFormatter = NumberFormatter()
-    let operandFormatter: NumberFormatter = NumberFormatter()
     
-    var firstOperandValue: Double = 0.0000
-    var secondOperandValue: Float = 0.0000
+    var firstOperandValue: Double = 1.0000
+    var secondOperandValue: Double = 1.0000
+    
+    let resultFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 4
+        formatter.minimumFractionDigits = 0
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    let operandFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 4
+        formatter.minimumFractionDigits = 4
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         let mainView = MainView(frame: CGRect.zero)
         self.view = mainView
-        self.firstOperandResultLabel = mainView.firstOperandView.resultLabel
-        self.secondOperandResultLabel = mainView.secondOperandView.resultLabel
+        self.firstOperand = mainView.firstOperandView.resultLabel
+        self.secondOperand = mainView.secondOperandView.resultLabel
         self.calculateButton = mainView.calculateButton
         self.resultLabel = mainView.resultLabel
-        self.firstOperandResultLabel.text = "1"
-        self.secondOperandResultLabel.text = "1"
-        self.resultLabel.text = "1"
         self.slider = mainView.secondOperandView.slider
         self.segmentedControl = mainView.firstOperandView.segmentedControl
         self.slider.minimumValue = 1
         self.slider.maximumValue = 100
-        self.slider.value = self.secondOperandValue
+        self.slider.value = Float(self.secondOperandValue)
         self.segmentedControl.minimumValue = 0
         self.segmentedControl.maximumValue = 10
         self.segmentedControl.stepValue = 0.5
-
         self.calculateButton.addTarget(self, action: #selector(self.calculate), for: .touchUpInside)
         self.slider.addTarget(self, action: #selector(self.changeSecondValue), for: .valueChanged)
         self.segmentedControl.addTarget(self, action: #selector(self.changeFirstValue), for: .valueChanged)
-        self.configureFormatter(formatter: self.resultFormatter, minDigits: 0)
-        self.configureFormatter(formatter: self.operandFormatter, minDigits: 4)
-        super.viewDidLoad()
+        self.printResult()
+        self.firstOperand.text = self.operandFormatter.string(from: NSNumber(value: self.firstOperandValue))
+        self.secondOperand.text = self.operandFormatter.string(from: NSNumber(value: self.secondOperandValue))
     }
     
-    private func configureFormatter(formatter: NumberFormatter, minDigits: Int) {
-        formatter.maximumFractionDigits = 4
-        formatter.minimumFractionDigits = minDigits
-        formatter.numberStyle = .decimal
+    private func printResult() {
+        self.resultLabel.text = self.resultFormatter.string(from: NSNumber(value: self.firstOperandValue * self.secondOperandValue))
     }
-    
-    @objc private func calculate(sender: UIButton!) {
-        self.resultLabel.text =
-            self.resultFormatter.string(from: NSNumber(value: Float(self.firstOperandValue) * self.secondOperandValue))
+
+    @objc func calculate(_ sender: UIButton!) {
+        printResult()
     }
-    
-    @objc private func changeSecondValue(sender: UISlider!) {
-        self.secondOperandValue = sender.value
-        self.secondOperandResultLabel.text = self.operandFormatter.string(from: NSNumber(value: self.secondOperandValue))
+
+    @objc func changeSecondValue(_ sender: UISlider!) {
+        self.secondOperandValue = Double(sender.value)
+        self.secondOperand.text = self.operandFormatter.string(from: NSNumber(value: self.secondOperandValue))
     }
-    
-    @objc private func changeFirstValue(sender: UIStepper!) {
+
+    @objc func changeFirstValue(_ sender: UIStepper!) {
         self.firstOperandValue = sender.value
-        self.firstOperandResultLabel.text = self.operandFormatter.string(from: NSNumber(value: self.firstOperandValue))
+        self.firstOperand.text = self.operandFormatter.string(from: NSNumber(value: self.firstOperandValue))
     }
 }
 
